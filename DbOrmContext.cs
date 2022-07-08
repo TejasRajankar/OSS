21071d4a3506748b845fdea5d11cb909e5a79b87
TejasRajankar <108930240+TejasRajankar@users.noreply.github.com>
2022-07-08 15:42:49 +0530

    Commit
    Addded the file

---
+++ DbOrmContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPIProject.Models;
+using System.Data.Entity;
+using MySql.Data.EntityFramework;
+
+namespace WebAPIProject.DAL
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class DbORMContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbORMContext() : base("WebAppCon")
+        {
+        }
+    }
+}
